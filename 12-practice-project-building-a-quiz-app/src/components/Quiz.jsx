d931445d3a9924e28e8419f15a06aef8b442f711
Rolucatelli <rolucatelli@gmail.com>
2025-08-26 00:01:36 -0300

    Started and Finished Section 12

---
+++ 12-practice-project-building-a-quiz-app/src/components/Quiz.jsx
@@ -0,0 +1,36 @@
+import { useCallback, useState } from "react";
+import QUESTIONS from "../questions";
+import Question from "./Question";
+import Summary from "./Summary";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  const quisIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback((selectedAnswer) => {
+    setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
+  }, []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quisIsComplete) {
+    return <Summary userAnswers={userAnswers} />;
+  }
+
+  return (
+    <div id='quiz'>
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
+}
